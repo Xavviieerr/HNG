b836638931b480961b7ec64f0197a84101b7fa66
Chidera paul ogbu <ogbupaul545@gmail.com>
2026-04-29 15:14:05 +0100

    feat(auth): implement auth middleware

---
+++ Backend/stage-1/advanced-name-classifier-api/src/middleware/roleMiddleware.js
@@ -0,0 +1,14 @@
+export const roleMiddleware = (allowedRoles) => {
+	return (req, res, next) => {
+		const userRole = req.user.role;
+
+		if (!allowedRoles.includes(userRole)) {
+			return res.status(403).json({
+				status: "error",
+				message: "Forbidden - insufficient permissions",
+			});
+		}
+
+		next();
+	};
+};
